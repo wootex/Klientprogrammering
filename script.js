49aac82758537bc496c5f24208b4f836ec257726
wootex <76428102+wootex@users.noreply.github.com>
2021-09-14 15:07:38 +0200

    dropdown button

---
+++ script.js
@@ -0,0 +1,12 @@
+// $(function(){
+    function menuPopup() {
+        var x = document.getElementById("menu");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+
+        }
+        else {
+            x.style.display = "none";
+        }
+    }
+// });
