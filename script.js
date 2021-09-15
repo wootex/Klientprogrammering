ceadb476723c0e6aae856ad1adf6868d39fe3b43
wootex <76428102+wootex@users.noreply.github.com>
2021-09-15 12:31:08 +0200

    mkt saker

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-// $(function(){
-    function menuPopup() {
-        var x = document.getElementById("menu");
-        if (x.style.display === "none") {
-            x.style.display = "block";
+$(document).ready(function(){
 
-        }
-        else {
-            x.style.display = "none";
-        }
+
+$("#menuImg").on("click", function (e) {
+    e.stopPropagation();
+    if ($("#menu").attr("class") === "hidden") {
+        $("#menu").addClass("show");
+        $("#menu").removeClass("hidden");
     }
-// });
+    else {
+        $("#menu").addClass("hidden");
+        $("#menu").removeClass("show");
+    }
+});
+
+/* automatic slideshow homepage */
+var slideIndex = 0;
+nextPic();
+function nextPic() {
+    var i;
+    var x = document.getElementsByClassName("slidePic");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) { slideIndex = 1 }
+    x[slideIndex - 1].style.display = "block";
+    setTimeout(nextPic, 2500);
+}
+
+
+
+});
