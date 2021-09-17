d3d542e2f1e4e29e4d559416c83d29afc6081635
Noel Lindgren <apexlindgren@hotmail.com>
2021-09-17 10:31:59 +0200

    Senaste ändringar

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
-$(document).ready(function(){
+// Körs när sidans DOM-träd har laddat klart
+$(document).ready(function () {
 
+    // Script för drop down från meny-ikon, visar och döljer med animering
+    $("#menuIcon").on("click", function () {
+        var dropdown = $("#wrapperDropdown");
 
-$("#menuImg").on("click", function (e) {
-    e.stopPropagation();
-    if ($("#menu").attr("class") === "hidden") {
-        $("#menu").addClass("show");
-        $("#menu").removeClass("hidden");
+        if (dropdown.attr("class") === "hidden") {
+            $("#menuIcon").addClass("open");
+            dropdown.removeClass("hidden");
+
+            dropdown.animate({
+                height: "100%"
+            }, 150, function () {
+                dropdown.addClass("show");
+            });
+        }
+        else {
+            dropdown.animate({
+                height: "0%"
+            }, 150, function () {
+                dropdown.addClass("hidden").removeClass("show");
+            });
+
+            $("#menuIcon").removeClass("open");
+        }
+
+    });
+
+    /* automatic slideshow (ska göras om till JQ och metoden .each() )*/
+    var slideIndex = 0;
+    nextPic();
+    function nextPic() {
+        var i;
+        var x = document.getElementsByClassName("slidePic");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > x.length) { slideIndex = 1 }
+        x[slideIndex - 1].style.display = "block";
+        setTimeout(nextPic, 2500);
     }
-    else {
-        $("#menu").addClass("hidden");
-        $("#menu").removeClass("show");
-    }
-});
-
-/* automatic slideshow homepage */
-var slideIndex = 0;
-nextPic();
-function nextPic() {
-    var i;
-    var x = document.getElementsByClassName("slidePic");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > x.length) { slideIndex = 1 }
-    x[slideIndex - 1].style.display = "block";
-    setTimeout(nextPic, 2500);
-}
-
-
 
 });
